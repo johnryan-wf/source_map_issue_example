bf35a7af3c7117a97a78c518aaac4aabecb858e4
John Ryan <john.ryan@workiva.com>
2016-03-15 13:35:51 -0500

    init

---
+++ web/main.dart
@@ -0,0 +1,48 @@
+library main;
+
+import 'dart:html';
+import 'dart:async';
+
+import 'package:stack_trace/stack_trace.dart';
+import 'package:source_map_stack_trace/source_map_stack_trace.dart';
+import 'package:source_maps/source_maps.dart';
+
+main() async {
+  var map = await getMapping();
+
+  Chain.capture(() async {
+    try {
+      await throwAsyncException();
+    } catch(e, stackTrace) {
+      var mapped = mapStackTrace(map, stackTrace);
+      print(mapped);
+      rethrow;
+    }
+  }, onError: (err, Chain chain) {
+    var cleaned = new Trace.from(chain);
+    cleaned = cleaned.foldFrames(
+        (frame) => frame.isCore,
+        terse: true);
+    print(cleaned);
+  });
+}
+
+Future<Mapping> getMapping() async {
+  var sourceMap = await HttpRequest.getString('trace_chain.dart.js.map');
+  return parse(sourceMap);
+}
+
+Future throwAsyncException() async {
+  var value = await _doStuff();
+  print('done! $value');
+}
+
+Future<int> _doStuff() async {
+  var result = await _doMoreStuff();
+  return result * 2;
+}
+
+Future<int> _doMoreStuff() async {
+  throw(new Exception('async exception'));
+  return 5;
+}
